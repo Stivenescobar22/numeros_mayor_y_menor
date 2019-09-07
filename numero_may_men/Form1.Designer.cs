c0c70660af13f3a781a25e009892edf33158d9cf
Stivenescobar22 <estibenes@gmail.com>
2019-09-07 18:18:44 -0500

    programa que calcula el numero mayor y el numero menor

---
+++ numero_may_men/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace numero_may_men
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.lblnumero1 = new System.Windows.Forms.Label();
+            this.lblnumero2 = new System.Windows.Forms.Label();
+            this.txtnumero1 = new System.Windows.Forms.TextBox();
+            this.txtnumero2 = new System.Windows.Forms.TextBox();
+            this.lblmayor = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.lblmenor = new System.Windows.Forms.Label();
+            this.lbltotal2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(279, 24);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(175, 26);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Calculo Numeros";
+            // 
+            // lblnumero1
+            // 
+            this.lblnumero1.AutoSize = true;
+            this.lblnumero1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero1.Location = new System.Drawing.Point(164, 101);
+            this.lblnumero1.Name = "lblnumero1";
+            this.lblnumero1.Size = new System.Drawing.Size(172, 22);
+            this.lblnumero1.TabIndex = 1;
+            this.lblnumero1.Text = "Ingrese un numero :";
+            // 
+            // lblnumero2
+            // 
+            this.lblnumero2.AutoSize = true;
+            this.lblnumero2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero2.Location = new System.Drawing.Point(164, 172);
+            this.lblnumero2.Name = "lblnumero2";
+            this.lblnumero2.Size = new System.Drawing.Size(172, 22);
+            this.lblnumero2.TabIndex = 2;
+            this.lblnumero2.Text = "Ingrese un numero :";
+            // 
+            // txtnumero1
+            // 
+            this.txtnumero1.Location = new System.Drawing.Point(354, 104);
+            this.txtnumero1.Name = "txtnumero1";
+            this.txtnumero1.Size = new System.Drawing.Size(148, 20);
+            this.txtnumero1.TabIndex = 3;
+            // 
+            // txtnumero2
+            // 
+            this.txtnumero2.Location = new System.Drawing.Point(354, 175);
+            this.txtnumero2.Name = "txtnumero2";
+            this.txtnumero2.Size = new System.Drawing.Size(148, 20);
+            this.txtnumero2.TabIndex = 4;
+            // 
+            // lblmayor
+            // 
+            this.lblmayor.AutoSize = true;
+            this.lblmayor.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmayor.Location = new System.Drawing.Point(258, 240);
+            this.lblmayor.Name = "lblmayor";
+            this.lblmayor.Size = new System.Drawing.Size(72, 22);
+            this.lblmayor.TabIndex = 5;
+            this.lblmayor.Text = "Mayor :";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotal.Location = new System.Drawing.Point(350, 240);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(52, 22);
+            this.lbltotal.TabIndex = 6;
+            this.lbltotal.Text = "Total";
+            this.lbltotal.Visible = false;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(539, 131);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(79, 34);
+            this.btncalcular.TabIndex = 7;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.Btncalcular_Click);
+            // 
+            // lblmenor
+            // 
+            this.lblmenor.AutoSize = true;
+            this.lblmenor.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmenor.Location = new System.Drawing.Point(258, 288);
+            this.lblmenor.Name = "lblmenor";
+            this.lblmenor.Size = new System.Drawing.Size(78, 22);
+            this.lblmenor.TabIndex = 8;
+            this.lblmenor.Text = "Menor : ";
+            // 
+            // lbltotal2
+            // 
+            this.lbltotal2.AutoSize = true;
+            this.lbltotal2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotal2.Location = new System.Drawing.Point(354, 288);
+            this.lbltotal2.Name = "lbltotal2";
+            this.lbltotal2.Size = new System.Drawing.Size(52, 22);
+            this.lbltotal2.TabIndex = 9;
+            this.lbltotal2.Text = "Total";
+            this.lbltotal2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltotal2);
+            this.Controls.Add(this.lblmenor);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.lblmayor);
+            this.Controls.Add(this.txtnumero2);
+            this.Controls.Add(this.txtnumero1);
+            this.Controls.Add(this.lblnumero2);
+            this.Controls.Add(this.lblnumero1);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Label lblnumero1;
+        private System.Windows.Forms.Label lblnumero2;
+        private System.Windows.Forms.TextBox txtnumero1;
+        private System.Windows.Forms.TextBox txtnumero2;
+        private System.Windows.Forms.Label lblmayor;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label lblmenor;
+        private System.Windows.Forms.Label lbltotal2;
+    }
+}
+
---
+++ numero_may_men/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numero_may_men
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btncalcular_Click(object sender, EventArgs e)
+        {
+
+            string num1 = txtnumero1.Text, num2 = txtnumero2.Text;
+           
+            int numero1 = int.Parse(num1);
+            int numero2 = int.Parse(num2);
+
+            if (numero1 > numero2) {
+
+
+
+                lbltotal.Text = numero1.ToString();
+                lbltotal2.Text = numero2.ToString();
+                lbltotal.Visible = true;
+                lbltotal2.Visible = true;
+                
+
+            } else {
+
+                if (numero2 > numero1) {
+
+                    lbltotal.Text = numero2.ToString();
+                    lbltotal2.Text = numero1.ToString();
+                    lbltotal.Visible = true;
+                    lbltotal2.Visible = true;
+
+
+                }
+                else {
+
+                    lbltotal.Text = " Son iguales ";
+                    lbltotal2.Text = " Son iguales ";
+                    lbltotal.Visible = true;
+                    lbltotal2.Visible = true;
+
+
+                }
+
+
+            }
+
+
+
+        }
+    }
+}
